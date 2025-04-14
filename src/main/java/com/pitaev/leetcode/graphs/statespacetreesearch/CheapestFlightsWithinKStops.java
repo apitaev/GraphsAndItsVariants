aa02a40a979808508aeb14a65a9b3346c557080b
Anna Pitaev <anna.pitaev@gmail.com>
2025-04-14 16:33:55 -0700

    Updated solution for the TreeSearchAlgorithm

--- src/main/java/com/pitaev/leetcode/graphs/statespacetreesearch/CheapestFlightsWithinKStops.java
+++ src/main/java/com/pitaev/leetcode/graphs/statespacetreesearch/CheapestFlightsWithinKStops.java
@@ -12,69 +12,65 @@ import java.util.Comparator;
  * Therefore nodes will be stored multiple times in the PriorityQueue. The node with the min distance, will be returned froom the queue.
  */
 class CheapesetFlightsWithinKStops {
-    static class Stop {
-        int node;
-        int distance;
-        int cost;
-        int level;
+        static class Stop {
+            Integer node;
+            final Integer distance;
+            Integer cost;
+            Integer level;
 
-        Stop(int  node, int cost) {
-            this.node = node;
-            this.cost = cost;
-        }
-
-        int getDistance() {
-            return this.distance;
-        }
-
-        @Override
-        public boolean equals(Object object) {
-            Stop stop = (Stop) object;
-            return this.node == stop.node && this.distance == stop.distance && this.cost == stop.cost && this.level == stop.level;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(node, distance, cost, level);
-        }
-    }
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        Map<Integer, List<Stop>> adjMap = buildGraph(n, flights);
-        // build graph
-        PriorityQueue<Stop> queue = new PriorityQueue<>(Comparator.comparingInt(Stop::getDistance));
-        queue.offer(new Stop(src, 0));
-        // run Dijkstra based on the distance information
-        while(queue.size() > 0) {
-            Stop current = queue.poll();
-            // if node == dst exit, return distance after the queue only, to make sure it is the minimum distance
-            if (current.node == dst) {
-                return current.distance;
+            Stop(Integer node, Integer cost) {
+                this.node = node;
+                this.cost = cost;
+                this.distance = 0;
             }
-            // if level == k + 1, skip this node, we can not exit here, it might be nodes with smaller levels and bigger costs in the queue
-            if (current.level <= k) {
+            Stop(Integer node, Integer cost, Integer distance, Integer level) {
+                this.node = node;
+                this.cost = cost;
+                this.distance = distance;
+                this.level = level;
+            }
+
+            Stop withDistanceAndLevel(Integer distance, Integer level) {
+                return new Stop(this.node, this.cost, distance, level);
+            }
+
+        }
+        public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+            Map<Integer, List<Stop>> adjMap = buildGraph(n, flights);
+            // build graph
+            PriorityQueue<Stop> queue = new PriorityQueue<>((a, b) -> a.distance.compareTo(b.distance));
+            Stop source = new Stop(src, 0, 0, 0);
+            queue.offer(source);
+            // run Dijkstra based on the distance information
+            while(queue.size() > 0) {
+                Stop current = queue.poll();
+                System.out.println(current.node + " " + current.distance);
+                // if node == dst exit
+                if (current.node.equals(dst))  {
+                    return current.distance;
+                }
+                // if level == k + 1, skip this node
+                if (current.level.equals(k + 1)) continue;
                 for (Stop stop : adjMap.get(current.node)) {
-                    stop.distance = current.distance + stop.cost;
-                    stop.level = current.level + 1;
-                    queue.offer(stop);
+                    Stop stopForQueue = stop.withDistanceAndLevel(current.distance + stop.cost, current.level + 1);
+                    queue.offer(stopForQueue);
                 }
             }
+            return -1;
         }
-        return -1;
-    }
 
-    private static Map<Integer, List<Stop>> buildGraph(Integer n, int [][] flights) {
-        Map<Integer, List<Stop>> result = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            result.put(i, new ArrayList<>());
+        private static Map<Integer, List<Stop>> buildGraph(Integer n, int [][] flights) {
+            Map<Integer, List<Stop>> result = new HashMap<>();
+            for (int i = 0; i < n; i++) {
+                result.put(i, new ArrayList<>());
+            }
+            for (int [] flight : flights) {
+                Integer name = flight[0];
+                Integer neighbor = flight[1];
+                Integer cost = flight[2];
+                Stop stop = new Stop(neighbor, cost);
+                result.get(name).add(stop);
+            }
+            return result;
         }
-        for (int i = 0; i < flights.length; i++) {
-            int [] flight = flights[i];
-            int name = flight[0];
-            int neighbor = flight[1];
-            int cost = flight[2];
-            Stop stop = new Stop(neighbor, cost);
-            result.get(name).add(stop);
-        }
-        return result;
     }
-}
